e8446b9456f5a2f2b16eb3da453e1fd0ee761ca2
khanhquang97 <chang.gunny@gmail.com>
2018-03-27 21:26:53 +0700

    dung co tranh nua

--- bai1.java
+++ bai1.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class bai1 {
 	
-	public static void main(String[] args) {
+	
 		Scanner keyboard=new Scanner(System.in);
 		System.out.println("a=");
 System.out.println("a=");System.out.println("a=");System.out.println("a=");
